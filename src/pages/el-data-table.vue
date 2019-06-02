41395c58145c94507e4389a498d97bf1dc540bc8
快乐风男 <413255589@qq.com>
2019-06-02 18:59:45 +0800

    vue的ssh渲染一个table

---
+++ src/pages/el-data-table.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="index">
+    <el-data-table v-bind="tableConfig" ></el-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableConfig: {
+        url: '/mock/11/components',
+        columns: [
+          { type: 'selection'},
+          {
+            prop: 'name',
+            label: '组件名称'
+          },
+          {
+            prop: 'category',
+            label: '分类'
+          },
+          {
+            prop: 'version',
+            label: '版本'
+          },
+          {
+            prop: 'lang',
+            label: '开发语言'
+          },
+          {
+            prop: 'last_up_time',
+            label: '最后更新时间'
+          },
+          {
+            prop: 'state',
+            label: '状态',
+            formatter: row => (row.state === 'normal' ? '上架' : '下架')
+          }
+        ],
+        searchForm: [
+          {
+            $type: 'input',
+            $id: 'name',
+            label: '组件名称',
+            $el: {
+              placeholder: '请输入'
+            }
+          },
+          {
+            $type: 'select',
+            $id: 'category',
+            label: '分类',
+            $el: {
+              placeholder: '请输入'
+            }
+          },
+          {
+            $type: 'select',
+            $id: 'state',
+            label: '状态',
+            $el: {
+              placeholder: '请输入'
+            }
+          }
+        ],
+        form: [
+          {
+            $type: 'input',
+            $id: 'name',
+            label: '组件名',
+            $el: {
+              placeholder: '请输入'
+            },
+            rules: [
+              {
+                required: true,
+                message: '请输入用户名',
+                trigger: 'blur'
+              }
+            ]
+          }
+        ],
+        extraButtons: [
+          {
+            type: 'danger',
+            text: '删除'
+          }
+        ]
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+<style lang="less">
+.index {
+  .home-img {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    <router-link to="el-data-table">Go2Table</router-link>
+    <img class="home-img" :src="$store.state.meta.homePageImg" alt="homeImg" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style lang="less">
+.index {
+  .home-img {
+    width: 100%;
+  }
+}
+</style>
